5c66f89023c16b5bf22051972b85030099f073b9
Sarthak Agarwal <57105611+Sarthak-ONS@users.noreply.github.com>
2023-06-11 21:37:19 +0530

    Added Themes

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '75 Hard',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      themeMode: ThemeMode.system,
+      darkTheme: ThemeData(),
       home: const HomeScreen(),
     );
   }
